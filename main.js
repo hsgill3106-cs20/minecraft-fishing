587d0e382c98ac4cf9e69a7849eefbeefcc51245
hsgill3106 <108752326+hsgill3106@users.noreply.github.com>
2022-07-08 11:32:42 -0600

    completed

--- main.js
+++ main.js
@@ -24,40 +24,34 @@ resetBtn.addEventListener("click", resetBtnClicked);
 function fishBtnClicked() {
   // Check Selected Character
   let character = charSelect.value;
-
+ 
   // Catch fish based on character
   if (character === "steve") {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = "img/Raw-Cod.png";
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = "img/Raw-Salmon.png";
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = "img/Tropical-Fish.png";
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = "img/Pufferfish.png";
-    }
+    catchFish(0.7, 0.9, 0.95);
   } else if (character === "alex") {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
+    catchFish(0.1, 0.2, 0.5);
+  } else if (character === "Villager") {
+
+    //VILLAGER: ALL 25%
+    catchFish(0.25, 0.5, 0.75);
+  }
+}
+
+
+function catchFish(codChance, salmChance, tropChance) {
+
+  let randNum = Math.random();
+    if (randNum < codChance) {
       numCod++;
       numCodSpan.innerHTML = numCod;
       resultImg.src = "img/Raw-Cod.png";
-    } else if (randNum < 0.2) {
+    } else if (randNum < salmChance) {
       numSalmon++;
       numSalmonSpan.innerHTML = numSalmon;
       resultImg.src = "img/Raw-Salmon.png";
-    } else if (randNum < 0.5) {
+    } else if (randNum < tropChance) {
       numTropical++;
       numTropicalSpan.innerHTML = numTropical;
       resultImg.src = "img/Tropical-Fish.png";
@@ -66,7 +60,6 @@ function fishBtnClicked() {
       numPufferSpan.innerHTML = numPuffer;
       resultImg.src = "img/Pufferfish.png";
     }
-  }
 }
 
 // Event handler for resetBtn
